5d5762a8afd47d94b448a3aa0b483b0c7236ba7b
Saeid Mohadjer <saeid@fastmail.fm>
2018-07-24 16:10:52 +0200

    Fixes bug in quantity in step 3

--- app/steps/3.php
+++ app/steps/3.php
@@ -1,5 +1,4 @@
 <form class="fromwizard__ajaxForm formwizard__step" method="post" action="xt_form.php?step=3">
-	<h2>Step 3</h2>
 	<?php
 		if ( isset($_SESSION["error"]) && $_SESSION["error"] ) {
 			echo '<p class="error">There is an error on the form!</p>';
@@ -8,6 +7,7 @@
 	<?php
 		if ( isset($_GET["mode"]) && ( trim($_GET["mode"]) == 'add' ) ) {
 	?>
+			<h2>Step 3 - sub-step 2</h2>
 			<div>
 				<label class="required" for="code">Item Code:</label>
 				<input type="text" id="code" name="code" required="required" value="" />
@@ -23,6 +23,8 @@
 	<?php
 		} else {
 	?>
+
+			<h2>Step 3 - sub-step 1</h2>
 			<table border="1">
 				<tr>
 					<th>Item Code</th>
@@ -31,7 +33,7 @@
 	<?php
 		if ( isset($_SESSION["items"]) ) {
 			foreach ( $_SESSION["items"] as $item ) {
-				echo "<tr><td>{$item['code']}</td><td>{$item['code']}</td></tr>";
+				echo "<tr><td>{$item['code']}</td><td>{$item['qty']}</td></tr>";
 			}
 		}
 	?>
